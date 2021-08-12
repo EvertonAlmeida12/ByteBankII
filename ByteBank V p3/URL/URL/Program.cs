d9128d49111e24a4012e54649d2eb69e369cc58e
Everton Almeida <65305306+EvertonAlmeida12@users.noreply.github.com>
2021-08-11 23:59:32 -0300

    Add files via upload

---
+++ ByteBank V p3/URL/URL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pagina?argumentos
+            //01234567
+
+            string url = "pagina?argumentos";
+            Console.WriteLine(url);
+
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
