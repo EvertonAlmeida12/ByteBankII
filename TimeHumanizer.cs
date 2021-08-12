c25ba7acf1c5bfe2a4e6aba875dc6e527eb85775
Everton Almeida <65305306+EvertonAlmeida12@users.noreply.github.com>
2021-08-12 00:00:33 -0300

    Add files via upload

---
+++ TimeHumanizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+
+namespace ByteBank.SistemaAgencia
+{
+    class TimeHumanizer
+    {
+
+        static void main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 6, 30);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40);  //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca) + ".";
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
